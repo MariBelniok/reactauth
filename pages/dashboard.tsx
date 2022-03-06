d6b546748f9fb25e54009e9cb43e58577bfe756c
maribelniok <mariannabelniok@outlook.com>
2022-03-06 09:19:30 -0300

    tratativas de erro nos tokens

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,9 +7,9 @@ export default function Dashboard() {
     const {user } = useContext(AuthContext)
 
     useEffect(() => {
-        api.get('/me').then(response => console.log(response))
-
-        console.log(parseCookies());
+        api.get('/me')
+            .then(response => console.log(response))
+            .catch(err => console.log(err));
     }, [])
 
     return (
